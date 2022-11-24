56b1daa0e1bc4c5fcd47c7ac6f211ee8ab305575
Kiryl <bartosian1989@gmail.com>
2022-11-24 10:46:11 -0500

    Update formatting.

--- check_tps.sh
+++ check_tps.sh
@@ -2,6 +2,7 @@
   
 echo '-------------------------------------'
 echo $(date)
+echo 
 SUISTART=$(curl --location --request POST https://fullnode.devnet.sui.io:443 \
 --header 'Content-Type: application/json' \
 --data-raw '{ "jsonrpc":"2.0", "method":"sui_getTotalTransactionNumber","id":1}' 2>/dev/null | jq .result)
@@ -10,9 +11,6 @@ NODESTART=$(curl --location --request POST 127.0.0.1:9000 \
 --header 'Content-Type: application/json' \
 --data-raw '{ "jsonrpc":"2.0", "method":"sui_getTotalTransactionNumber","id":1}' 2>/dev/null | jq .result)
 
-echo 'SUI START TXS COUNT: '$SUISTART
-echo 'NODE START TXS COUNT: '$NODESTART
-
 for I in {1..10}; do
   sleep 1
   BAR="$(yes . | head -n ${I} | tr -d '\n')"
@@ -20,8 +18,6 @@ for I in {1..10}; do
 done
 printf "\n"
 
-echo '-------------------------------------'
-echo $(date)
 SUIEND=$(curl --location --request POST https://fullnode.devnet.sui.io:443 \
 --header 'Content-Type: application/json' \
 --data-raw '{ "jsonrpc":"2.0", "method":"sui_getTotalTransactionNumber","id":1}' 2>/dev/null | jq .result)
@@ -32,8 +28,7 @@ NODEEND=$(curl --location --request POST 127.0.0.1:9000 \
 
 SUITPS=$((($SUIEND-$SUISTART)/10))
 MYTPS=$((($NODEEND-$NODESTART)/10))
-echo 'SUI END TXS COUNT: '$SUIEND
-echo 'NODE END TXS COUNT: '$NODEEND
-echo '-------------------------------------'
+
 echo 'SUI TPS: '$SUITPS
 echo 'NODE TPS: '$MYTPS
+echo '-------------------------------------'
