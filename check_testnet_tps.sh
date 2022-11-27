97bdad87b9076a3d317c681524ae1351f7751d4c
Kiryl <bartosian1989@gmail.com>
2022-11-27 13:28:32 -0500

    Added check for jq package in testnet script

--- check_testnet_tps.sh
+++ check_testnet_tps.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
   
+JQ_OK=$(dpkg-query -W --showformat='${Status}\n' jq|grep "install ok installed")
+if [ "" = "$PKG_OK" ]; then
+  sudo apt update
+  sudo apt install -y jq
+fi
+
 echo '-------------------------------------'
 echo $(date)
 echo 
